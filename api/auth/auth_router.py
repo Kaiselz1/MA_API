b9ce8f777a591a2b5ba760acdbde3ecab9953253
Nhngobhz <realbunthai@gmail.com>
2026-01-18 19:22:28 +0700

    change the messages when wrong login details

--- api/auth/auth_router.py
+++ api/auth/auth_router.py
@@ -38,16 +38,17 @@ def register(request: auth_schema.UserSignupRequest, db: Session = Depends(get_d
 def login(request: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):       
     user = db.query(user_model.User).filter(user_model.User.email == request.username).first()
     if not user:
+        print("User not found")
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Invalid Credentials"
         )
     if not hash.verify(user.password, request.password):
+        print("Incorrect password")
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail="Incorrect Password"
+            detail="Invalid Credentials"
         )
-    
     access_token = token.create_access_token(
         data={"sub": user.email}
     )
